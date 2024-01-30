f555d12036e0f20aff1a760e6c27040f0457460f
phubestp <phubest.p@ku.th>
2024-01-31 01:40:34 +0700

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <Navbar />
-          {children}
+        <Navbar />
+        <div>{children}</div>
       </body>
     </html>
   );
